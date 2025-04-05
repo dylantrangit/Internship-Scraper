6d597520028b860e9105feda2e0fb3148a3c7e38
dylan tran <dytran@student.unimelb.edu.au>
2025-04-05 22:25:54 +1100

    Everythin is working, until I got pinged on the UNSW scraper for bot activity, will look further

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gc_sel import gradConnectScraper, gradConnectLogIn
+from unsw_sel import unswScraper
 from selenium import webdriver
+import undetected_chromedriver as uc
 import psycopg2
 
 connection = psycopg2.connect(
@@ -10,13 +12,23 @@ connection = psycopg2.connect(
     port="5432"
 )
 
-URL = "https://au.gradconnection.com/internships/computer-science/australia/"
-URL_LOGIN = "https://au.gradconnection.com/login/"
+URL_GC = "https://au.gradconnection.com/internships/computer-science/australia/"
+URL_LOGIN_GC = "https://au.gradconnection.com/login/"
 USERNAME = "notascraper36@gmail.com"
 PASSWORD = "@Confirmpassword123"
-driver = webdriver.Chrome()
+driver_GC = webdriver.Chrome()
+
+URL_UNSW = "https://careers.business.unsw.edu.au/search-jobs?locations=9692&study_fields=502&opportunity_types=2&defaults_applied=1"
+options = uc.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless")
+
+driver_UNSW = uc.Chrome(options=options)
 
 
-gradConnectLogIn(driver, URL_LOGIN, USERNAME, PASSWORD)
-gradConnectScraper(driver, URL, connection)
+gradConnectLogIn(driver_GC, URL_LOGIN_GC, USERNAME, PASSWORD)
+gradConnectScraper(driver_GC, URL_GC, connection)
+unswScraper(driver_UNSW, URL_UNSW, connection)
 
--- unsw_sel.py
+++ unsw_sel.py
@@ -1,42 +1,44 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-import undetected_chromedriver as uc
 import time
 
-URL = "https://careers.business.unsw.edu.au/search-jobs?locations=9692&study_fields=502&opportunity_types=2&defaults_applied=1"
-options = uc.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options)
-driver.get(URL)
-print(driver.title)
-time.sleep(5)
+def unswScraper(driver, URL, connection):
+    cur = connection.cursor()
+    driver.get(URL)
+    print(driver.title)
+    time.sleep(5)
 
-links_to_visit = []
+    links_to_visit = []
 
-#all_lists = driver.find_elements(By.CLASS_NAME, "SearchResultsstyle__SearchResult-sc-c560t5-1 hlOmzw")/
-all_lists = driver.find_elements(By.CSS_SELECTOR, "li.SearchResultsstyle__SearchResult-sc-c560t5-1.hlOmzw")
-for list in all_lists:
-    list_link = list.find_element(By.TAG_NAME, 'a')
-    links_to_visit.append(list_link.get_attribute('href'))
+    all_lists = driver.find_elements(By.CSS_SELECTOR, "li.SearchResultsstyle__SearchResult-sc-c560t5-1.hlOmzw")
+    for list in all_lists:
+        list_link = list.find_element(By.TAG_NAME, 'a')
+        links_to_visit.append(list_link.get_attribute('href'))
 
 
-for link in links_to_visit:
-    driver.get(link)
+    for link in links_to_visit:
+        driver.get(link)
 
-    title = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.TAG_NAME, "h1"))
-    ).text
-    unsw_URL = link
-    company = driver.find_element(By.XPATH, "//ul[@class='breadcrumbs']/li[3]/a/span").text
-    location = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[3]/div/div/div[2]/div/div/section/div[1]/section/div[1]/div/p').text
-    deadline = driver.find_element(By.XPATH, "//span[starts-with(text(), 'Apply by')]").text
+        title = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, "h1"))
+        ).text
+        unsw_URL = link
+        company = driver.find_element(By.XPATH, "//ul[@class='breadcrumbs']/li[3]/a/span").text
+        location = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[3]/div/div/div[2]/div/div/section/div[1]/section/div[1]/div/p').text
+        deadline = driver.find_element(By.XPATH, "//span[starts-with(text(), 'Apply by')]").text
 
-    print(deadline)
 
-driver.quit()
+        data = (title, unsw_URL, company, location, deadline)
+        query = """
+        INSERT INTO internships_unsw (title, url, company, city, deadline)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        cur.execute(query, data)
+        connection.commit()
+        
+        
+
+
+    driver.quit()
